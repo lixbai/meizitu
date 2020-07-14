4c342c68cd38744f95caa51ec56c60fd5a3abd0a
lixbai <1090205084@qq.com>
2020-07-14 18:25:00 +0800

    update

--- apps/album/views.py
+++ apps/album/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'album/index.html')
+    return render(request, 'download/download.html')
 
 def albums(request):
     return render(request, 'album/beauty_album.html')
